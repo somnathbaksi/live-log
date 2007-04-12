db6ac09c31f3b4b29fb781faa315f793ca07299e
zl3143 <zl3143@c12f5fe7-c821-0410-a9a2-fdfa40e65d69>
2007-04-12 11:28:38 +0000

    add membership table in db

--- Xps/PublishArticle.aspx
+++ Xps/PublishArticle.aspx
@@ -4,6 +4,6 @@
 
 <%@ Register Assembly="FredCK.FCKeditorV2" Namespace="FredCK.FCKeditorV2" TagPrefix="FCKeditorV2" %>
 <asp:Content ID="Content1" ContentPlaceHolderID="ContentPlaceHolder1" runat="Server">
-    <FCKeditorV2:FCKeditor ID="FCKeditor1" runat="server" BasePath="~/FCKeditor/" OnDataBinding="FCKeditor1_DataBinding">
+    <FCKeditorV2:FCKeditor ID="FCKeditor1" runat="server" BasePath="~/FCKeditor/" OnDataBinding="FCKeditor1_DataBinding" >
     </FCKeditorV2:FCKeditor>
 </asp:Content>
--- Xps/PublishArticle.aspx.cs
+++ Xps/PublishArticle.aspx.cs
@@ -8,6 +8,8 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using System.Web.UI.WebControls.WebParts;
 using System.Web.UI.HtmlControls;
+using Live5.Xps.Framework;
+using Live5.Xps.ArticleComponent;
 
 public partial class PublishArticle : System.Web.UI.Page
 {
@@ -18,6 +20,16 @@ public partial class PublishArticle : System.Web.UI.Page
 
     protected void FCKeditor1_DataBinding(object sender, EventArgs e)
     {
-     
+        bool success=PubArticle();
+        Trace.Write(FCKeditor1.Value);
+    }
+    bool PubArticle()
+    {
+        Article entry = new Article();
+        entry.Summary = "summary";
+        entry.Title = "title";
+        entry.Content = FCKeditor1.Value;
+        PublishService p = new PublishService();
+        return p.PutEntry(entry);
     }
 }
